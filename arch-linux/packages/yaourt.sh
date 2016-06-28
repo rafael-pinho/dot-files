5af3f076e045a8c71073da62262d6633e5c9e092
Rafael Pinho <rp.rafaelpinho@gmail.com>
2016-06-27 22:13:52 -0300

    Update yaourt.sh

--- arch-linux/packages/yaourt.sh
+++ arch-linux/packages/yaourt.sh
@@ -1,9 +1,9 @@
 yaourt -S               	\
 	chromium-pepper-flash 	\
-	playerctl								\
-	spotify									\
-	nvm-git									\
-	visual-studio-code			\
-	libcurl-compat					\
+	playerctl		\
+	spotify			\
+	nvm-git			\
+	visual-studio-code	\
+	libcurl-compat		\
 	vte-notification-common \
 	vte3-notification
