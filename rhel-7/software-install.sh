87464ff9b351c6032742dec715bbcd44a3996d94
rafael <rp.rafaelpinho@gmail.com>
2016-07-13 22:20:13 -0300

    adição scripts rhel

---
+++ rhel-7/software-install.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+function get_repos(){
+	sudo curl https://raw.githubusercontent.com/rafael-pinho/dot-files/master/rhel-7/repo-list/docker.repo > /etc/yum.repo.d/
+	sudo curl https://raw.githubusercontent.com/rafael-pinho/dot-files/master/rhel-7/repo-list/google-chrome.repo > /etc/yum.repo.d/
+	sudo curl https://raw.githubusercontent.com/rafael-pinho/dot-files/master/rhel-7/repo-list/google.repo > /etc/yum.repo.d/
+	sudo curl https://raw.githubusercontent.com/rafael-pinho/dot-files/master/rhel-7/repo-list/opera.repo > /etc/yum.repo.d/
+	sudo curl https://raw.githubusercontent.com/rafael-pinho/dot-files/master/rhel-7/repo-list/epel.repo > /etc/yum.repo.d/
+	sudo curl https://raw.githubusercontent.com/rafael-pinho/dot-files/master/rhel-7/repo-list/epel-testing.repo > /etc/yum.repo.d/
+	sudo curl https://raw.githubusercontent.com/rafael-pinho/dot-files/master/rhel-7/repo-list/mirrors-rpmforge > /etc/yum.repo.d/
+	sudo curl https://raw.githubusercontent.com/rafael-pinho/dot-files/master/rhel-7/repo-list/mirrors-rpmforge-extras > /etc/yum.repo.d/
+	sudo curl https://raw.githubusercontent.com/rafael-pinho/dot-files/master/rhel-7/repo-list/mirrors-rpmforge-testing > /etc/yum.repo.d/
+	sudo curl https://raw.githubusercontent.com/rafael-pinho/dot-files/master/rhel-7/repo-list/rpmforge.repo > /etc/yum.repo.d/
+	
+	echo "Press any key to continue..."
+        read
+}
+
+function install_browser(){
+	echo "Select browser"
+	echo "1- Google Chrome"
+	echo "2- Opera"
+	read BROWSER
+  
+	case $BROWSER in
+	  1)
+	    sudo yum install google-chrome-stable
+	    ;;
+	  2)
+	    sudo yum install opera
+	    ;;
+	esac
+		
+	echo "Press any key to continue..."
+	read
+}
+
+function install_developer_tools(){
+	sudo yum install git docker
+
+	curl https://raw.githubusercontent.com/creationix/nvm/v0.30.2/install.sh | bash
+	source ~/.bashrc
+	
+	sudo yum install golang
+	
+	echo "Press any key to continue..."
+        read
+}
+
+while [ "$USER_INPUT" != ":quit" ]; do
+
+clear
+printf "\n\n"
+echo "***********************************************************************"
+echo "*********************   RHEL-7 SOFTWARE SCRIPT   **********************"
+echo "***********************************************************************"
+printf "\n\n"
+echo "choose what you want to do"
+printf "\n"
+echo "0 ............... all"
+echo "1 ............... get repos"
+echo "2 ............... browser"
+echo "3 ............... git, nvm, golang and docker"
+printf "\n\n:quit - EXIT\n\n"
+read USER_INPUT
+
+case $USER_INPUT in
+	0)
+	    get_repos
+            install_browser
+	    install_developer_tools
+	    ;;
+	1)
+	    get_repos
+	    ;;
+	2)
+	    install_browser
+	    ;;
+	3)
+	    install_developer_tools
+	    ;;
+esac
+
+done
+
