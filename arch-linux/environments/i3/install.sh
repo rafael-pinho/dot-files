5f953fb8401b45b71df83358e94bc93c1a3cc1c5
Rafael Pinho <rp.rafaelpinho@gmail.com>
2016-08-21 23:46:06 -0300

    Update install.sh

--- arch-linux/environments/i3/install.sh
+++ arch-linux/environments/i3/install.sh
@@ -7,3 +7,5 @@ echo \#\!\/bin\/bash > temp
 sed "s/\#\!\/bin\/bash/exec i3/g" /etc/X11/xinit/xinitrc >> temp
 cat temp > /etc/X11/xinit/xinitrc
 rm temp
+
+curl https://raw.githubusercontent.com/rafael-pinho/dot-files/master/arch-linux/environments/i3/i3.conf > /etc/i3/config
