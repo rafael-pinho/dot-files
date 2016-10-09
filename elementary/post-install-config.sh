a77b1f16a7cff53b2c4166055391969f1a453dc1
Rafael Pinho <rp.rafaelpinho@gmail.com>
2016-10-09 20:50:31 -0300

    Update post-install-config.sh

--- elementary/post-install-config.sh
+++ elementary/post-install-config.sh
@@ -3,13 +3,20 @@
 apt-get update
 apt-get upgrade -y
 
+
 apt-get install software-properties-common \
                 gdebi \
                 ubuntu-restricted-extras
 
+
 add-apt-repository ppa:philip.scott/elementary-tweaks
 add-apt-repository ppa:numix/ppa
+
+wget http://download.opensuse.org/repositories/home:Horst3180/xUbuntu_16.04/Release.key
+sudo apt-key add - < Release.key
+sudo apt-get update
 echo 'deb http://download.opensuse.org/repositories/home:/Horst3180/xUbuntu_16.04/ /' >> /etc/apt/sources.list.d/arc-theme.list
+
 apt-get update -y
 
 apt-get install elementary-tweaks \
