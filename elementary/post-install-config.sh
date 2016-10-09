d572e95e90578667f9bfee41afe7541222d404d3
Rafael Pinho <rp.rafaelpinho@gmail.com>
2016-10-09 20:57:25 -0300

    Update post-install-config.sh

--- elementary/post-install-config.sh
+++ elementary/post-install-config.sh
@@ -8,7 +8,8 @@ apt-get upgrade -y
 #install some util packages
 apt-get install software-properties-common \
                 gdebi \
-                ubuntu-restricted-extras
+                ubuntu-restricted-extras \
+                -y
 
 
 #add elementary tweaks, numix and arc theme repositories
@@ -20,13 +21,18 @@ apt-key add - < Release.key
 apt-get update
 echo 'deb http://download.opensuse.org/repositories/home:/Horst3180/xUbuntu_16.04/ /' >> /etc/apt/sources.list.d/arc-theme.list
 
-apt-get update -y
+
+#add spotify repository
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
+echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
 
 
-#install tweaks, numix and arch themes
+#install tweaks, numix, arch themes and spotify
+apt-get update
 apt-get install elementary-tweaks \
                 numix-gtk-theme \
                 numix-icon-theme-circle \ 
                 arc-theme \
+                spotify-client \
                 -y 
                 
