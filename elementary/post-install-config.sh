00ef65f1f3ce88082d898e38c5ec895ceec56118
Rafael Pinho <rp.rafaelpinho@gmail.com>
2016-10-09 20:50:54 -0300

    Update post-install-config.sh

--- elementary/post-install-config.sh
+++ elementary/post-install-config.sh
@@ -13,8 +13,8 @@ add-apt-repository ppa:philip.scott/elementary-tweaks
 add-apt-repository ppa:numix/ppa
 
 wget http://download.opensuse.org/repositories/home:Horst3180/xUbuntu_16.04/Release.key
-sudo apt-key add - < Release.key
-sudo apt-get update
+apt-key add - < Release.key
+apt-get update
 echo 'deb http://download.opensuse.org/repositories/home:/Horst3180/xUbuntu_16.04/ /' >> /etc/apt/sources.list.d/arc-theme.list
 
 apt-get update -y
