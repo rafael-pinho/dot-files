191422f3cbec8b907b7c1f8a3df4859d3aaa078c
Rafael Pinho <rp.rafaelpinho@gmail.com>
2016-10-09 20:52:42 -0300

    Update post-install-config.sh

--- elementary/post-install-config.sh
+++ elementary/post-install-config.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+#update system
 apt-get update
 apt-get upgrade -y
 
 
+#install some util packages
 apt-get install software-properties-common \
                 gdebi \
                 ubuntu-restricted-extras
 
 
+#add elementary tweaks, numix and arc theme repositories
 add-apt-repository ppa:philip.scott/elementary-tweaks
 add-apt-repository ppa:numix/ppa
 
@@ -19,6 +22,8 @@ echo 'deb http://download.opensuse.org/repositories/home:/Horst3180/xUbuntu_16.0
 
 apt-get update -y
 
+
+#install tweaks, numix and arch themes
 apt-get install elementary-tweaks \
                 numix-gtk-theme \
                 numix-icon-theme-circle \ 
